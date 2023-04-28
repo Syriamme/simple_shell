35422f04a2f114ec8e0fb6c59bbdb3ed0d615773
Syriamme <roridaniel01@gmail.com>
2023-04-28 02:05:11 -0700

    the bash hell gates

--- getting_environs.c
+++ getting_environs.c
@@ -12,19 +12,17 @@ int my_set_env(list_y **environ, char **pointer)
 	int ixe = 0;
 	list_y *hdr;
 	char *cpyca;
-	
+
 	if (pointer[1] == NULL || pointer[2] == NULL)
 	{
 		write(STDOUT_FILENO, "few args\n", 7);
 		double_ptr_free(pointer);
 		return (-1);
 	}
-	
 	cpyca = my_str_dp(pointer[1]);
 	cpyca = my_stringcat(cpyca, "=");
 	cpyca = my_stringcat(cpyca, pointer[2]);
 	ixe = my_gtenv(*environ, pointer[1]);
-	
 	if (ixe == -1)
 	{
 		end_node_add(environ, cpyca);
