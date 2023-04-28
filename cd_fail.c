307dd7e788f641d67d1991b45a7a788da5cddfb4
Syriamme <Roridaniel01@gmail.com>
2023-04-28 09:43:45 +0300

    the bash hell gate

--- cd_fail.c
+++ cd_fail.c
@@ -13,39 +13,32 @@ void cd_fail(char *pointer, int nt, list_y *environ)
 	char *sel;
 	char *nuber;
 	int sum =0;
-
-	sel = my_get_enr("_", environ);
 	
+	sel = my_get_enr("_", environ);
 	while (sel[sum] != '\0')
 		sum++;
 	write(STDOUT_FILENO, sel, sum);
 	free(sel);
-	
 	write(STDOUT_FILENO, ": ", 2);
 	nuber = integ_string(nt);
-	
 	for (sum = 0; nuber[sum] != '\0'; sum++)
 		;
-	
 	write(STDOUT_FILENO, nuber, sum);
 	free(sel);
-	
 	write(STDOUT_FILENO, ": ", 2);
 	write(STDOUT_FILENO, "cd: cannot cd ", 14);
-	
 	for (sum = 0; pointer[sum] != '\0'; sum++)
 		;
 	write(STDOUT_FILENO, pointer, sum);
-	
 	write(STDOUT_FILENO, "\n", 1);
 }
 
 
 /**
- * _wrong_number - error for wrong number
- * @str: command typed
+ *_wrong_number- error for wrong number
+ * @pointer: command typed
  * @nt: the command after cmd
- * @eniron: the envt var
+ * @environ: the envt var
  */
 
 void _wrong_number(char *pointer, int nt, list_y *environ)
@@ -55,25 +48,18 @@ void _wrong_number(char *pointer, int nt, list_y *environ)
 	int sum = 0;
 	
 	sel = my_get_enr("_", environ);
-	
 	while (sel[sum] != '\0')
 		sum++;
-	
 	write(STDOUT_FILENO, sel, sum);
 	free(sel);
-	
 	write(STDOUT_FILENO, ": ", 2);
 	nuba = integ_string(nt);
-	
 	for (sum = 0; nuba[sum] != '\0'; sum++)
 		write(STDOUT_FILENO, nuba, sum);
 	free(nuba);
-	
 	write(STDOUT_FILENO, ": ", 2);
 	write(STDOUT_FILENO, "exit: wrong number: ", 20);
-	
 	for (sum = 0; pointer[sum] != '\0'; sum++)
 		write(STDOUT_FILENO, pointer, sum);
-	
 	write(STDOUT_FILENO, "\n", 1);
 }
